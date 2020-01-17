c01acec919aae3159ac5fdc85db40aa180e0f668
Student <example@example.com>
2020-01-17 10:01:33 -0800

    debug, page has '?'

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,31 @@
+// Back-end
+
+var add = function(nature, travel, animal, water, freeTime) {
+  return nature + travel + animal + water + freeTime;
+};
+
+// Front-end
+
 $(document).ready(function() {
-  $("form#language-survey").submit(function(event){
+  $("form#languageSurvey").click(function(event){
+    event.preventDefault();
     var nature = parseInt($("select#nature").val());
     var travel = parseInt($("select#travel").val());
     var animal = parseInt($("select#animal").val());
     var water = parseInt($("select#water").val());
     var freeTime = parseInt($("select#freetime").val());
+    
     var result = add(nature, travel, animal, water, freeTime);
-    $("output").text(result);
-  
-    event.preventDefault();
+    
+    // $("output").append
+    
+    alert(result);
+
+
+
+    // $(#ruby).show();
+    // $(#csharp).show();
+    // $(#python).show();
+    
   });
 });
